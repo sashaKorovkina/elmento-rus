3bbc94428cbed108ee165fb8b148a3fd399da0ae
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-05-12 16:01:02 +0100

    control test

--- Elmento.py
+++ Elmento.py
@@ -1,3 +1,14 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
+
 # from openai import OpenAI
 # import streamlit as st
 #
