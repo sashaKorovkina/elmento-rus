7ba1bf200fe4ab0f4eaec324602a09e95deddbb2
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-05-12 17:52:23 +0100

    control test

--- pages/3_Чат_с_ИИ.py
+++ pages/3_Чат_с_ИИ.py
@@ -86,10 +86,10 @@ if 'logged_in' in st.session_state and st.session_state.logged_in:
                     st.write(f"Начало чат-сессии для: {selected_chat_data['filename']}")
                 # st.write(f"The id in the selected file is: {selected_chat_data['chat_id']}")
                     display_messages(selected_chat_data['chat_id'], username)
-                    if prompt := st.chat_input("Что вас интересует?"):
-                        chat_id = selected_chat_data['chat_id']
-                        with st.chat_message("user"):
-                            st.markdown(prompt)
+                if prompt := st.chat_input("Что вас интересует?"):
+                    chat_id = selected_chat_data['chat_id']
+                    with st.chat_message("user"):
+                        st.markdown(prompt)
                 #     #st.session_state.messages.append({"role": "user", "content": prompt})
                 #             response = response_func(prompt, selected_chat_data['pdf_text'])
                 #             with st.chat_message("assistant"):
