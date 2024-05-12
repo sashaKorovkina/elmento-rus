0a45ef9e5533ffb9f6548da8a5f7ef31f81e59ad
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-05-12 21:45:56 +0100

    control test

--- pages/3_Чат_с_ИИ.py
+++ pages/3_Чат_с_ИИ.py
@@ -91,27 +91,47 @@ if 'logged_in' in st.session_state and st.session_state.logged_in:
 
             if selected_chat_data:
                 st.write(f"Начало чат-сессии для: {selected_chat_data['filename']}")
-                # st.write(f"The id in the selected file is: {selected_chat_data['chat_id']}")
                 display_messages(selected_chat_data['chat_id'], username)
 
-                # Chat input
-                if prompt := st.chat_input("Что вас интересует?"):
-                    chat_id = selected_chat_data['chat_id']
-                    with st.chat_message("user"):
-                        st.markdown(prompt)
-                    # st.session_state.messages.append({"role": "user", "content": prompt})
+                prompt = st.text_input("Что вас интересует?")
+                if st.button("Отправить запрос"):
+                    if prompt:
+                        chat_id = selected_chat_data['chat_id']
+                        with st.spinner("Ответ на ваш запрос обрабатывается..."):
+                            st.write(f"Вы: {prompt}")
 
-                    # AI response
-                    response = response_func(prompt, selected_chat_data['pdf_text'])
-                    with st.spinner("Ответ на ваш запрос обрабатывается..."):
-                        st.write(response)
-                        doc_ref = db.collection('users').document(username).collection('chats').document(
-                            chat_id).collection('messages').document()
-                        doc_ref.set({
-                            'message_user': prompt,
-                            'message_ai': response,
-                            'timestamp': datetime.datetime.now(datetime.timezone.utc).isoformat()
-                        })
+                            # AI response
+                            response = response_func(prompt, selected_chat_data['pdf_text'])
+                            st.write(f"ИИ: {response}")
+
+                            # Save user message and AI response to database
+                            doc_ref = db.collection('users').document(username).collection('chats').document(
+                                chat_id).collection('messages').document()
+                            doc_ref.set({
+                                'message_user': prompt,
+                                'message_ai': response,
+                                'timestamp': datetime.datetime.now(datetime.timezone.utc).isoformat()
+                            })
+                    else:
+                        st.error("Пожалуйста, введите запрос перед отправкой.")
+                # # Chat input
+                # if prompt := st.chat_input("Что вас интересует?"):
+                #     chat_id = selected_chat_data['chat_id']
+                #     with st.chat_message("user"):
+                #         st.markdown(prompt)
+                #     # st.session_state.messages.append({"role": "user", "content": prompt})
+                #
+                #     # AI response
+                #     response = response_func(prompt, selected_chat_data['pdf_text'])
+                #     with st.spinner("Ответ на ваш запрос обрабатывается..."):
+                #         st.write(response)
+                #         doc_ref = db.collection('users').document(username).collection('chats').document(
+                #             chat_id).collection('messages').document()
+                #         doc_ref.set({
+                #             'message_user': prompt,
+                #             'message_ai': response,
+                #             'timestamp': datetime.datetime.now(datetime.timezone.utc).isoformat()
+                #         })
 else:
     st.write('Пожалуйста, войдите в систему или зарегистрируйтесь, чтобы просмотреть эту страницу.')
 
