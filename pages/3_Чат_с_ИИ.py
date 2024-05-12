22344aeab2e9fb4225eb0cfe6d8f672917392270
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-05-12 22:47:55 +0100

    container width side chats

--- pages/3_Чат_с_ИИ.py
+++ pages/3_Чат_с_ИИ.py
@@ -108,8 +108,8 @@ if 'logged_in' in st.session_state and st.session_state.logged_in:
                                 selected_question_type = question_type
 
                     # Further processing based on the selected question type
-                    # if selected_question_type:
-                    #     st.write(f"Вы выбрали тип вопроса: {selected_question_type}")
+                    if selected_question_type:
+                        st.write(f"Вы выбрали тип вопроса: {selected_question_type}")
 
                     if prompt := st.chat_input("Что вас интересует?"):
                         chat_id = selected_chat_data['chat_id']
