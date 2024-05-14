859dc854b3b0160807b01f45c8d144128009d2f3
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-05-14 13:24:40 +0100

    docx to img

--- pages/2_Файлы.py
+++ pages/2_Файлы.py
@@ -270,7 +270,11 @@ def pdf_parse_content(pdf_bytes, language):
         pdf_image = Image.open(io.BytesIO(image_data))
         pdf_images.append(pdf_image)
 
-        text = pytesseract.image_to_string(pdf_image, lang = 'rus')
+        if language == 'Russian':
+            lang = 'rus'
+        elif language == 'English':
+            lang = 'eng'
+        text = pytesseract.image_to_string(pdf_image, lang = lang)
         pdf_texts.append(text)
 
     st.session_state['username'] = username
