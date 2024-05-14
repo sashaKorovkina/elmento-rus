8ac089639c725ab82392b66e9109f68ffe720c0f
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-05-14 22:30:46 +0100

    docx to img

--- pages/3_Чат_с_ИИ.py
+++ pages/3_Чат_с_ИИ.py
@@ -30,7 +30,7 @@ def response_func(prompt, text):
 
     chain = load_qa_chain(llm, chain_type="stuff")
     with get_openai_callback() as cb:
-        result = chain.run(input_documents=docs, question=prompt, max_tokens=4000)
+        result = chain.run(input_documents=docs, question=prompt)
     return result
 
 
