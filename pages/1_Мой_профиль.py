9422fa2c1a70eb86923a2e88ad32937b7e671e01
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-07-06 11:03:01 +0100

    docx to img

--- pages/1_Мой_профиль.py
+++ pages/1_Мой_профиль.py
@@ -75,7 +75,7 @@ if not st.session_state['signedout']:
     st.session_state.db = db
 #     docs = db.collection('users').get()
 
-    choice = st.selectbox('Регистрация/Вход', ['Вход', 'Регистрация'])
+    choice = st.selectbox('Регистрация/Вход', ['Регистрация', 'Вход'])
 
     if choice == 'Вход':
         email = st.text_input('Адрес электронной почты')
