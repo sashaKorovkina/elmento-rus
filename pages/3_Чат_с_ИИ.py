ccde82bec2bc60fc65a9017b2ca509d79f9ba7f9
sashaKorovkina <sasha.korovkina2003@gmail.com>
2024-07-22 14:06:13 +0100

    docx to img

--- pages/3_Чат_с_ИИ.py
+++ pages/3_Чат_с_ИИ.py
@@ -27,7 +27,7 @@ def response_func(prompt, text):
         embeddings = OpenAIEmbeddings(openai_api_key = api_key)
         knowledge_base = FAISS.from_texts(chunks, embeddings)
         docs = knowledge_base.similarity_search(prompt)
-        llm = OpenAI(model_name="gpt-3.5-turbo-instruct", openai_api_key=api_key, max_tokens=1200)
+        llm = OpenAI(model_name="gpt-3.5-turbo", openai_api_key=api_key, max_tokens=1200)
         #OpenAI(temperature=1, max_tokens=1000)
 
         chain = load_qa_chain(llm, chain_type="stuff")
